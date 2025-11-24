f3283319dbd1f97931afc756ff38ac1bd5495b82
Morgueniadt <145459977+Morgueniadt@users.noreply.github.com>
2025-11-24 15:40:26 +0000

    new repo CA2

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/LoginForm";
+
+export default function Home({loggedIn, onLogin}) {
+    return (
+        <>
+            <h1>This is Home</h1>
+            
+            {!loggedIn && <LoginForm onLogin={onLogin} />}
+            
+        </>
+    );
+};
